56555b03d59f304aeefb923fb481988ecbb0a804
Edward Wages <125240723+Edward-Wages@users.noreply.github.com>
2025-04-15 16:54:52 -0500

    Final Submission

--- PPL_Homework6.py
+++ PPL_Homework6.py
@@ -1,4 +1,8 @@
 import re
+
+#####   Edward Wages - Homework 6 - Python Project: A text lsit to an HTML Table    #####
+
+
 #For this project, we need to wrtie a python program to output an HTML file titled "output.html" that describes a table. This table should
 #list studetn records, with one student per row. Each record will contain a last name, first name, ID number, & letter grade. This table will be sorted
 #by Last name, then first name and ID
@@ -8,8 +12,7 @@ import re
 #Each ID is a 9-digit integer & no duplicate IDs exist
 #The only module that should be importes id re (regular expression)
 
-#I think we can start by making a student class with a variable for each of the values
-class Student:
+class Student: #We'll have a student class which will store each variable field once we've acquired them
     def __init__(self, firstName, lastName, grade, ID, eagerness):
         self.firstName = firstName
         self.lastName = lastName
@@ -24,26 +27,25 @@ class Student:
 
     def __reprwithGrades__(self):
         return f"{self.firstName} {self.lastName} {self.grade} {self.ID} {self.eagerness} {self.letterGrade}"
-
-    def _printGrades_(self):
-        return f"{self.letterGrade}"
+    #Exact same thing was repr but with grades - can be ignored, was just used to check that variables are stored correctly
 
     def sort_students_final(student_list):
         return sorted(student_list, key=lambda x: (x.lastName, x.firstName, x.ID))
+    #Method which does the final sorting of the list according to project specifications
 
-    @classmethod
+    @classmethod    #This method wouldn't work without specifying as a class method, don't really understand why but it works :)
     def assign_letter_grades(cls, student_list):
-        if len(student_list) < 7:
+        if len(student_list) < 7: #The project specifies we should not be working with a class of less than 7 students
             raise ValueError("Class must have at least 7 students")
             
         # Sort by grade (descending) and eagerness (E comes first)
         sorted_students = sorted(student_list, 
                                key=lambda x: (-x.grade, 0 if x.eagerness == 'E' else 1))
         
-        n = len(sorted_students)
+        n = len(sorted_students) #Preparing variables ahead of time for the grade range
         a_cutoff = n // 3
         b_cutoff = 2 * (n // 3)
-        f_cutoff = n - ((n + 9) // 10)  # Proper ceiling for n/10
+        f_cutoff = n - ((n + 9) // 10) 
         
         # Assign grades
         for i, student in enumerate(sorted_students):
@@ -59,7 +61,7 @@ class Student:
 
 studentList = [] #Will be filled with Student objects once the records are created
 
-#We have an initial student class. From here we can probably start reading in the file & manipulating the strings to obtain the data we need
+#We have an initial student class. From here we can  start reading in the file & manipulating the strings to obtain the data we need
 with open("input.txt", "r") as file:
     #reads the whole file
     content = file.read()
@@ -69,13 +71,8 @@ with open("input.txt", "r") as file:
 #The information for each student is in a specific style; that being
         #   ID - FIRSTNAME - LASTNAME - GRADE - EAGERNESS - LOCATION(OPTIONAL)
 
-    #What if I did a split() function?
-list = re.split("\s",content)
-#So I think I just made this really easy with this...
-#Since we know the format of the file, we could probably loop thrugh & create our list of students from there
-
 #To figure out the amount of objects we need to create, we can use the findall function in re to find how many ID numbers exist in content, due to the ID
-#numbers having several static characteristics
+#numbers having several static characteristics we can take advantage of
 
 #Specific characteristics to utilize - The entire thing contains integer characters, and is exactly 9 characters long
 idList = re.findall("[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]",content)
@@ -104,27 +101,84 @@ matches = re.findall(r'START(.*?)END', superTrimmedContent, flags=re.DOTALL)
 # 1. Replace any internal whitespace (including newlines) with single space
 # 2. Strip surrounding whitespace
 clean_matches = [' '.join(match.split()) for match in matches]
-#I AM A GENIUS AMONGST MEN WHO HAS THOUGHT OF THIS BEFORE
 
 firstNames = []
+#list of each student's first name
 lastNames = []
+#Self explanatory
 
+#Loop to assign the names
 for full_name in clean_matches:
     parts = full_name.split()
 
     firstNames.append(parts[0])
     lastNames.append(parts[1])
 
-
-
-#from here we can create x amount of student objects, where x is the number of ids found / the length of idList & assign everything directly
+#from here we can create x amount of student objects, where x is the number of ids found / the length of idList & assign everything directly.
+#Technically this could be accomplished with any of the lists, but I went with IDlist since that's the once I figured out how to do first
 for i in range(len(idList)):
     studentList.append(Student(firstName=firstNames[i], lastName=lastNames[i], grade=gradeList[i], ID= idList[i], eagerness=eagernessList[i]))
 
 #Next we assign the letter grades of the students
 Student.assign_letter_grades(studentList)
 
-Student.sort_students_final(studentList)
+sorted_Records = Student.sort_students_final(studentList)
 #Sorts the student list as specified in the homework document (Last name, first name, ID)
 
 #Now we'll start writing our findings to the HTML file
+
+with open("output.html", "w") as file:
+    # Write HTML header and table structure
+    file.write("""<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Grades</title> 
+    <style>
+        table 
+        {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td 
+        {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+        th 
+        {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <h1>Student Grades</h1>
+    <table>
+        <tr>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>ID</th>
+            <th>Letter Grade</th>
+        </tr>
+""")
+
+    # Write each student's data as a table row
+    for student in sorted_Records:
+        file.write(f"""        <tr>
+            <td>{student.ID}</td>
+            <td>{student.firstName}</td>
+            <td>{student.lastName}</td>
+            <td>{student.letterGrade}</td>
+        </tr>
+""")
+
+    # Close the HTML tags
+    file.write("""    </table>
+</body>
+</html>""")
+    
+#That's the whole thing! Some edge cases which I have attempted to cover for:
+    #Class being less than 7 students
+    #Students with similar names will be sorted by ID
+    #Don't need to worry too much about similar grades since the project specifies that ties will always be broken by eagerness
+#Have a good day!
