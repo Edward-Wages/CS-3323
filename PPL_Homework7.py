25ae06e8e84adb52a5dd62d9971322b4cb0e06cc
Edward Wages <125240723+Edward-Wages@users.noreply.github.com>
2025-04-16 18:02:29 -0500

    Power generator made, developed is_Prime( )

--- PPL_Homework7.py
+++ PPL_Homework7.py
@@ -8,15 +8,30 @@
 # We call an integer SuPrP2 if it can be written as a sum of a prime and a power of 2. For example, 
 # 15 is a SuPrP2 since 15 = 13 + 21, but 16 is not
 
+#Helper Methods
+# is_prime(n) was developed in class on 4/15/2025. It takes a number & checks if it is prime by computing the square of all values before n.
+def is_prime(n):
+    if n < 2:
+        return False
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
 
 
-#Write a Python generator that yields all powers of two starting from 1 (2^0)
-    #ALL powers of 2? That's gonna result in a lot of ridiculous numbers
-    #I guess that means we need to loop from 1 to MAX_INT? Is that even an option without importing modules?
-    #In a (infinite?) loop, generate all powers of two
-    #Naturally, we can do this in a limited loop & append each result into an array, then return that array
+#Write a Python generator that yields all powers of two starting from 1 (2^0) (DONE)
+def powersOf2Generator():
+    i = 1 #initial value of 1 / 2^0
+    while True:     #Provides allows us to infinitely acquire the next power 
+        yield 2 ** i 
+        i += 1
 
 #Write a Python generator that on an input (positive integer) n, yields all SuPrP2
 #numbers in the increasing order that are greater than n.
+def allSuPrP2_Nums(n):
+    #Needs a while True
+    #NEeds the rest of the function lmao
 
 #Let N be your student ID number (113540137). Use the generator to find 20 consecutive SuPrP2 right after N
