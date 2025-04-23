bb9762296c1952876c5d24b91859d5e38a959ec7
Edward Wages <125240723+Edward-Wages@users.noreply.github.com>
2025-04-22 20:39:09 -0500

    Final Submission

--- PPL_Homework7.py
+++ PPL_Homework7.py
@@ -30,8 +30,36 @@ def powersOf2Generator():
 
 #Write a Python generator that on an input (positive integer) n, yields all SuPrP2
 #numbers in the increasing order that are greater than n.
+#We know that each SuPrP2 number is in the formula x = p (prime) + 2^k
 def allSuPrP2_Nums(n):
-    #Needs a while True
-    #NEeds the rest of the function lmao
+    x = n + 1
+    while True:
+        #Check if x can be written as p + 2^k
+        for power in powersOf2Generator():
+            if power >= x:
+                break
+            p = x - power
+            if is_prime(p):
+                yield x
+                break
+        x += 1
+
 
 #Let N be your student ID number (113540137). Use the generator to find 20 consecutive SuPrP2 right after N
+
+def generateFirstTwentySuPrP2(n):
+    generator = allSuPrP2_Nums(n)
+    list = []
+    while len(list) < 20:
+        list.append(next(generator))
+    return list
+
+
+#Driver code
+n = 113540137
+finalList = generateFirstTwentySuPrP2(n)
+print(finalList)
+
+#The answer for question 3 will be posted as a comment in the source code - source code will be submitted as a text file
+#[113540139, 113540145, 113540147, 113540149, 113540151, 113540153, 113540155, 113540157, 113540161, 113540163, 
+# 113540165, 113540167, 113540169, 113540171, 113540173, 113540175, 113540177, 113540179, 113540181, 113540185]
